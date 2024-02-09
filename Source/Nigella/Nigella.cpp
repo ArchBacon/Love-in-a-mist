7845ad9d4582f529e7847c0b6cff504a075f2e0d
Christian <c.delicaat@hotmail.com>
2024-02-10 00:08:42 +0100

    Initial commit

---
+++ Source/Nigella/Nigella.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Nigella.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Nigella, "Nigella" );
